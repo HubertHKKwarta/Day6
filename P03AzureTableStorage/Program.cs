0a4216f41eb3007c9e8e616490440dea6bc18ad9
huber <huber@HUBERT>
2023-05-16 10:36:02 +0200

    Add project files.

---
+++ P03AzureTableStorage/Program.cs
@@ -0,0 +1,46 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=volleyballsa;AccountKey=BtD9aTXIs21vo+uIUdZwNB7BddWBCwSjhsucb6DaxfkWFZ7QT1SLweM+4GfYO52c01ckfw+RX4hS+AStclaibg==;EndpointSuffix=core.windows.net";
+
+
+
+string tableName = "Persons";
+
+var  tableClient = new TableClient(connectionString, tableName);
+
+//Azure.Data.Tables
+
+//// Create a table in your storage account
+await tableClient.CreateIfNotExistsAsync();
+//// Insert an entity into the table
+//var entity = new TableEntity("volleyball", "player5")
+//    {
+//        { "firstname", "john" },
+//        { "country", "BRA" },
+//        { "phonenumber", "123456" },
+//    };
+//await tableClient.AddEntityAsync(entity);
+//Console.WriteLine("Entity added to the table.");
+
+
+// Query entities from the table
+//var entity = new TableEntity("volleyball", "player6");
+//string partitionKeyFilter = $"PartitionKey eq '{entity.PartitionKey}'";
+//await foreach (var e in tableClient.QueryAsync<TableEntity>(partitionKeyFilter))
+//{
+//    Console.WriteLine($"" +
+//        $"PartitionKey: {e.PartitionKey}," +
+//        $" RowKey: {e.RowKey}," +
+//        $" Property1: {e.GetString("country")}, " +
+//        $" Property2: {e.GetString("firstname")}, " +
+//        $" Property3: {e.GetString("lastname")}, "); 
+
+
+//}
+//updating
+//var entity = new TableEntity("volleyball", "player5");
+//entity["country"] = "NewValue1";
+//await tableClient.UpdateEntityAsync(entity, ETag.All);
+//Console.WriteLine("Entity updated in the table.");
+
---
+++ P06/Program.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Storage.Files.DataLake;
+using Microsoft.Extensions.Configuration;
+
+
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=volleyballsa;AccountKey=BtD9aTXIs21vo+uIUdZwNB7BddWBCwSjhsucb6DaxfkWFZ7QT1SLweM+4GfYO52c01ckfw+RX4hS+AStclaibg==;EndpointSuffix=core.windows.net";
+var containerName = "image";
+
+//var connectionString = "myconnection";
+//var containerName = "mystoragecontainer";
+
+
+var dataLakeServiceClient = new DataLakeServiceClient(connectionString);
+var dataLakeContainerClient = dataLakeServiceClient.GetFileSystemClient(containerName);
+
+Console.WriteLine("Enter the file path of the image to upload:");
+var imagePath = Console.ReadLine();
+
+using var fileStream = File.OpenRead(imagePath);
+var blobName = Path.GetFileName(imagePath);
+
+var blobClient = dataLakeContainerClient.GetFileClient(blobName);
+await blobClient.UploadAsync(fileStream, true);
+Console.WriteLine($"Image '{imagePath}' uploaded successfully to Data Lake Storage Gen2.");
+
+Console.WriteLine("Enter the name of the image to download from Data Lake Storage Gen2:");
+var imageNameToDownload = Console.ReadLine();
+
+var downloadBlobClient = dataLakeContainerClient.GetFileClient(imageNameToDownload);
+Console.WriteLine("Enter the destination file path to save the downloaded image:");
+var destinationPath = Console.ReadLine();
+
+var download = await downloadBlobClient.ReadAsync();
+using var downloadFileStream = File.OpenWrite(destinationPath);
+
+await download.Value.Content.CopyToAsync(downloadFileStream);
+downloadFileStream.Close();
+Console.WriteLine($"Image '{imageNameToDownload}' downloaded successfully from Data Lake Storage Gen2 and saved to '{destinationPath}'.");
